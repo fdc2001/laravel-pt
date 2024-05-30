cc0240b78a1fe62d482fcd3c17de14a99b464ff5
Filipe Clemente <dev@filipeclemente.pt>
2024-05-30 11:52:01 +0100

    Update Localization, Add Event Carousel and Debugbar
    This commit updates the localization by deleting the LocalizationMiddleware and adding akaunting language package. Additionally, a new event carousel was implemented on the welcome page using Swiper.js for better user experience. Lastly, laravel debugbar was added for efficient debugging.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,28 +85,6 @@
                             </div>
                         </div>
                     </div>
-                    <div class="absolute flex justify-center items-center m-auto left-0 right-0 w-fit bottom-12">
-                        <button id="slider-button-left"
-                                class="swiper-button-prev group !p-2 flex justify-center items-center border border-solid border-indigo-600 !w-12 !h-12 transition-all duration-500 rounded-full  hover:bg-indigo-600 !-translate-x-16"
-                                data-carousel-prev>
-                            <svg class="h-5 w-5 text-indigo-600 group-hover:text-white"
-                                 xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"
-                                 fill="none">
-                                <path d="M10.0002 11.9999L6 7.99971L10.0025 3.99719" stroke="currentColor"
-                                      stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        </button>
-                        <button id="slider-button-right"
-                                class="swiper-button-next group !p-2 flex justify-center items-center border border-solid border-indigo-600 !w-12 !h-12 transition-all duration-500 rounded-full hover:bg-indigo-600 !translate-x-16"
-                                data-carousel-next>
-                            <svg class="h-5 w-5 text-indigo-600 group-hover:text-white"
-                                 xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"
-                                 fill="none">
-                                <path d="M5.99984 4.00012L10 8.00029L5.99748 12.0028" stroke="currentColor"
-                                      stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round"/>
-                            </svg>
-                        </button>
-                    </div>
                 </div>
             </div>
 
